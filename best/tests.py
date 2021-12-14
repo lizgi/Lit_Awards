de2b6d8946bdcf216c92650eb2b1fec56eb65275
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2021-12-14 16:07:35 +0300

    add tests.py

--- best/tests.py
+++ best/tests.py
@@ -1,3 +1,88 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from .models import  Profile,Project,Rating
 
-# Create your tests here.
+
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        user = User.objects.create(
+            username="test_user"
+        )
+
+        self.profile = Profile(
+            bio="Test profile_pic",
+            user=user,
+            contact="Test Contact",
+        )
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile, Profile))
+
+    def test_update_method(self):
+        self.profile.update_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+
+
+    def test_delete_method(self):
+        self.profile.save_profile()
+        self.profile.delete_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) == 0)
+
+    def test_save_method(self):
+        self.profile.save_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+
+    def test_create_method(self):
+        self.profile.create_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+
+class ProjectTestClass(TestCase):
+    def setUp(self):
+        user = User.objects.create(
+            username="test_user", first_name="elizabeth", last_name="nyambura"
+        )
+
+        self.project = Project(
+            title="Test Pics",
+            description="Test Category",
+            image="image.png",
+            user=user,
+        )
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.project, Project))
+
+    def test_save_method(self):
+        self.project.save_project()
+        projects = Project.objects.all()
+        self.assertTrue(len(projects) > 0)
+    
+
+    def test_update_project(self):
+        self.project.save_project()
+        projects = Project.objects.all()
+        self.assertTrue(len(projects) > 0)
+
+    def test_delete_method(self):
+        self.project.save_project()
+        self.project.delete_project()
+        projects = Project.objects.all()
+        self.assertTrue(len(projects) == 0)
+
+class RatingTestClass(TestCase):
+    def setUp(self):
+        user = User.objects.create(
+            username="test_user", first_name="elizabeth", last_name="nyambura"
+        )
+
+        self.rating = Rating(
+            user=user,
+        )
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.rating, Rating))        
+
