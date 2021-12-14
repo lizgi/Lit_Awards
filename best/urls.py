f7b5e53360bc837889b08974f5588301038c87fd
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2021-12-14 19:42:28 +0300

    push to heroku

--- best/urls.py
+++ best/urls.py
@@ -1,6 +1,7 @@
 from . import views
 from django.conf.urls import url
 from django.urls import path
+from django_registration.backends.one_step.views import RegistrationView
 
 urlpatterns=[
     path('',views.index,name = 'index'),
@@ -9,7 +10,7 @@ urlpatterns=[
     path('post/project/', views.showProject,name='project'),
     path('update_profile/<int:id>',views.update_profile, name='update_profile'),
     path('create_profile/',views.create_profile,name = 'create_profile'),
-    # path('accounts/register/', RegistrationView.as_view(success_url='/create_profile'),name='django_registration_register'),
+    path('accounts/register/', RegistrationView.as_view(success_url='/create_profile'),name='django_registration_register'),
     path("project/<int:project_id>/", views.project_details, name="project_details"),
     path('search/', views.search_project, name='search'),
     path('rate/<int:id>',views.rate, name='rate'),
